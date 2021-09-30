0bc197072f47bd42f305c8da78d8f829a6c2790c
simonaSoliman <simona.soliman@yahoo.com>
2021-09-30 20:47:18 +0200

    remove protectAPI until auth finished

--- config/routes.js
+++ config/routes.js
@@ -16,20 +16,22 @@ var validator = require('../config/validator.js');
 // const crypto = require('crypto');
 
 module.exports = function (app) {
-	console.log(
-"innnnn")
 	app.use(expressValidator(validator));
-
+	
+	/* add protectAPI to routes like:
+	*     app.get('/api_url', protectAPI, controller.method);
+	*  except auth and register
+	*/
 	app.post('/api/users/auth', authController.auth);
 	app.post('/api/users/register', authController.register);
-	app.get('/api/users/get_user/:id', protectAPI, authController.get_user);
+	app.get('/api/users/get_user/:id', authController.get_user);
 	app.post('/api/users/add_user', authController.add_user);
-	app.get('/api/users/get_group_users/:group_id', protectAPI, authController.get_group_users);
-	app.get('/api/users/search/:name/:group_id', protectAPI, authController.search);
+	app.get('/api/users/get_group_users/:group_id', authController.get_group_users);
+	app.get('/api/users/search/:name/:group_id', authController.search);
 
-	app.post('/api/groups/add_group',protectAPI, groupController.add_group );
-	app.get('/api/groups/get_groups',protectAPI, groupController.get_groups );
+	app.post('/api/groups/add_group', groupController.add_group );
+	app.get('/api/groups/get_groups', groupController.get_groups );
 
-	app.post('/api/attendance/add_attendance',protectAPI, attendanceController.add_attendance );
-	app.get('/api/attendance/get_group_attendance_by_date',protectAPI, attendanceController.get_group_attendance_by_date );
+	app.post('/api/attendance/add_attendance', attendanceController.add_attendance );
+	app.get('/api/attendance/get_group_attendance_by_date', attendanceController.get_group_attendance_by_date );
 };

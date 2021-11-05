de41b07111e041e06682b0f4142b192ba6b4248c
simonaSoliman <simona.soliman@yahoo.com>
2021-11-05 13:50:50 +0200

    update attendance record

--- app/controllers/attendanceController.js
+++ app/controllers/attendanceController.js
@@ -3,31 +3,64 @@ var Attendence = require('../models/attendence');
 module.exports = {
 
 	add_attendance: function (req, res) {
-        var new_attendance = {
-            date:new Date(req.body.date),
-            userId:req.body.userId,
-            groupId:req.body.groupId,
-            attend:req.body.attend,
-            note: req.body.note
-        };
-        var newAttendanceRecord = new Attendence(new_attendance);
-        newAttendanceRecord.save(function (err) {
-            if (err) {
-                var message = err
-                res.json({status: 422, title: message, type:"Error",details:message});
-                return;
-            }
+        const date = new Date(req.body.date);
 
-                res.json({
-                    state:true,
-                    status: 204,
-                    data: newAttendanceRecord, 
-                    message: 'attendance is created successfully'
-                });
-            
-        });  		
+        const today = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+        const tomorrow = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate()+1);
+        Attendence.findOne({
+            userId:req.body.userId,
+            groupId:req.body.groupId, date:{
+                $gte: new Date(today), 
+                $lt: new Date(tomorrow)
+        }}).exec(function(err, attendance){
+            const updated_attendance={
+                attend:req.body.attend,
+                note: req.body.note,
+            };
+            if(attendance){
+                Attendence.findOneAndUpdate({
+                    userId:req.body.userId,
+                    groupId:req.body.groupId, date:{
+                        $gte: new Date(today), 
+                        $lt: new Date(tomorrow)
+                }},{$set: updated_attendance },{new: true},function(err,attendance){
+                    if (err) {
+                        var message = err
+                        res.json({status: 422, title: message, type:"Error",details:message});
+                        return;
+                    }
+                    res.json({
+                        state:true,
+                        status: 204,
+                        data: attendance, 
+                        message: 'attendance is updated successfully'
+                    });     
+                })
+            }else{
+                var new_attendance = {
+                    date:new Date(req.body.date),
+                    userId:req.body.userId,
+                    groupId:req.body.groupId,
+                    attend:req.body.attend,
+                    note: req.body.note
+                };
+                var newAttendanceRecord = new Attendence(new_attendance);
+                newAttendanceRecord.save(function (err) {
+                    if (err) {
+                        var message = err
+                        res.json({status: 422, title: message, type:"Error",details:message});
+                        return;
+                    }
+                    res.json({
+                        state:true,
+                        status: 204,
+                        data: newAttendanceRecord, 
+                        message: 'attendance is created successfully'
+                    });
+                }); 
+            }
+        });    		
     },
-    
     get_group_attendance_by_date: function(req, res){
         const groupId = req.params.groupId;
         const today = req.params.today;

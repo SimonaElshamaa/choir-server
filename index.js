3a9655c4e3377bfaeca1c87fd12f0851e1608ae9
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-02-28 18:47:15 +0100

    remove negrok and add dotenv

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const logger = require('./logger');
 
 const argv = require('./argv');
 const port = require('./port');
-require('dotenv').config()
- 
+const dotenv = require('dotenv');
+dotenv.config();
+
 var bodyParser = require('body-parser');
 
 const isDev = process.env.NODE_ENV !== 'production';
-const ngrok =
-  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
-    ? require('ngrok')
-    : false;
 const app = express();
 const cors = require('cors');
 app.use(cors());
@@ -30,7 +27,7 @@ require('./config/routes')(app);
 require('./config/db');
 
 // get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.HOST;
+const customHost = process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
@@ -46,17 +43,5 @@ app.listen(port, host, async err => {
   if (err) {
     return logger.error(err.message);
   }
-
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    let url;
-    try {
-      url = await ngrok.connect(port);
-    } catch (e) {
-      return logger.error(e);
-    }
-    logger.appStarted(port, prettyHost, url);
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+  logger.appStarted(port, prettyHost);
 });

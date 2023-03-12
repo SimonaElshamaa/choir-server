8c13ccaaaf7e7be159d7d21a0b1f742e3dc04881
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-03-12 11:55:51 +0100

    remove /api

--- config/routes.js
+++ config/routes.js
@@ -22,17 +22,17 @@ module.exports = function (app) {
 	*     app.get('/api_url', protectAPI, controller.method);
 	*  except auth and register
 	*/
-	app.post('/api/users/auth', authController.auth);
-	app.post('/api/users/register', authController.register);
-	app.get('/api/users/get_user/:id', protectAPI, authController.get_user);
-	app.post('/api/users/add_user', protectAPI, authController.add_user);
-	app.get('/api/users/get_group_users/:groupId',protectAPI, authController.get_group_users);
-	app.get('/api/users/search/:name/:groupId', protectAPI, authController.search);
-	app.get('/api/users/get_me/:id', protectAPI, authController.get_me);
+	app.post('/users/auth', authController.auth);
+	app.post('/users/register', authController.register);
+	app.get('/users/get_user/:id', protectAPI, authController.get_user);
+	app.post('/users/add_user', protectAPI, authController.add_user);
+	app.get('/users/get_group_users/:groupId',protectAPI, authController.get_group_users);
+	app.get('/users/search/:name/:groupId', protectAPI, authController.search);
+	app.get('/users/get_me/:id', protectAPI, authController.get_me);
 
-	app.post('/api/groups/add_group', protectAPI, groupController.add_group );
-	app.get('/api/groups/get_groups', protectAPI, groupController.get_groups );
+	app.post('/groups/add_group', protectAPI, groupController.add_group );
+	app.get('/groups/get_groups', protectAPI, groupController.get_groups );
 
-	app.post('/api/attendances/add_attendance', protectAPI, attendanceController.add_attendance );
-	app.get('/api/attendances/get_group_attendance_by_date/:groupId/:today/:tomorrow',  protectAPI, attendanceController.get_group_attendance_by_date );
+	app.post('/attendances/add_attendance', protectAPI, attendanceController.add_attendance );
+	app.get('/attendances/get_group_attendance_by_date/:groupId/:today/:tomorrow',  protectAPI, attendanceController.get_group_attendance_by_date );
 };
